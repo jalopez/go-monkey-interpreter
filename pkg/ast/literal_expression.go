a367d97a18531455162c2aab2add9219713864d3
Javier López <lopez.pardo@gmail.com>
2023-12-04 21:47:57 +0100

    parse booleans

--- pkg/ast/literal_expression.go
+++ pkg/ast/literal_expression.go
@@ -15,3 +15,17 @@ func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
 
 // String string representation
 func (il *IntegerLiteral) String() string { return il.Token.Literal }
+
+// Boolean literal with boolean value
+type Boolean struct {
+	Token token.Token
+	Value bool
+}
+
+func (*Boolean) expressionNode() {} //nolint:golint,unused
+
+// TokenLiteral token literal
+func (b *Boolean) TokenLiteral() string { return b.Token.Literal }
+
+// String string representation
+func (b *Boolean) String() string { return b.Token.Literal }
