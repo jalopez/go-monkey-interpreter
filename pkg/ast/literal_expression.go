c745fcded0812560f40570f3b57f8fa7f44a4b8c
Javier López <lopez.pardo@gmail.com>
2023-12-14 21:59:35 +0100

    parse string literals

--- pkg/ast/literal_expression.go
+++ pkg/ast/literal_expression.go
@@ -39,3 +39,22 @@ func (b *Boolean) String() string { return b.Token.Literal }
 func (b *Boolean) ToJSON() string {
 	return `{"type":"boolean","value":` + b.Token.Literal + `}`
 }
+
+// StringLiteral literal with string value
+type StringLiteral struct {
+	Token token.Token
+	Value string
+}
+
+func (*StringLiteral) expressionNode() {} //nolint:golint,unused
+
+// TokenLiteral token literal
+func (sl *StringLiteral) TokenLiteral() string { return sl.Token.Literal }
+
+// String string representation
+func (sl *StringLiteral) String() string { return sl.Token.Literal }
+
+// ToJSON to json
+func (sl *StringLiteral) ToJSON() string {
+	return `{"type":"string","value":` + sl.Token.Literal + `}`
+}
